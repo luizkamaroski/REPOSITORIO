ad977b6a5e7d9c17fd88da1846065a3b1978c724
luizkamaroski <111889547+luizkamaroski@users.noreply.github.com>
2024-10-23 09:09:02 -0300

    Add files via upload

--- sei la man/graficos/quantidadedeUsuarioPorRede.js
+++ sei la man/graficos/quantidadedeUsuarioPorRede.js
@@ -49,4 +49,14 @@ async function quantidadeUsuariosPorRede() {
             color: getCSS('--secondary-color')
             }
             }
+            const getCSS = (variavel) => {
+                const bodyStyles = getComputedStyle(document.body)
+                return bodyStyles.getPropertyValue (variavel)
+                }
+                const tickConfig = {
+                family: getCSS('--font'),
+                size: 16,
+                color: getCSS('--primary-color')
+                }
+                export {getCSS, tickConfig}
             }
