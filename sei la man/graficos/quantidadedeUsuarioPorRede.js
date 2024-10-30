8220bbc0ba9a63df73c93e55ca764b05a26a6fd6
luizkamaroski <111889547+luizkamaroski@users.noreply.github.com>
2024-10-30 09:12:10 -0300

    Add files via upload

--- sei la man/graficos/quantidadedeUsuarioPorRede.js
+++ sei la man/graficos/quantidadedeUsuarioPorRede.js
@@ -1,62 +1,60 @@
+import { getCSS, tickConfig } from './common.js'
+
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json&#39'
-    const res = await fetch (url)
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados);
-    
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
     const data = [
         {
             x: nomeDasRedes,
             y: quantidadeDeUsuarios,
-            type: 'bar'
+            type: 'bar',
+            marker: {
+                color: 'blue'
+            }
         }
     ]
-    
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Ploty.newPlot(grafico,data)
-    
-    }
-    
-    quantidadeUsuariosPorRede();
 
-    const laytout = {
+
+    const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-        text: 'Redes sociais com mais usuários',
-        x: 0,
-        font: {
-        color: getCSS('--primary-color'),
-        size: 30,
-        font: getCSS('--font')
-        }
+            text: 'Redes sociais com mais usuários',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
         },
         xaxis: {
-        tickfont: tickConfig,
-        title: {
-        text: 'Nome das redes',
-        font: {
-        color: getCSS('--secondary-color')
-        }
-        }
+            tickfont: tickConfig,
+            title: {
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
         yaxis: {
             tickfont: tickConfig,
             title: {
-            text: 'Bilhões de usuários ativos',
-            font: {
-            color: getCSS('--secondary-color')
-            }
-            }
-            const getCSS = (variavel) => {
-                const bodyStyles = getComputedStyle(document.body)
-                return bodyStyles.getPropertyValue (variavel)
+                text: 'bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
                 }
-                const tickConfig = {
-                family: getCSS('--font'),
-                size: 16,
-                color: getCSS('--primary-color')
-                }
-                export {getCSS, tickConfig}
             }
+        }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+quantidadeUsuariosPorRede();
